f8100f94afe07c4aed6d4f72b719c008936a76b1
Katie Kowalyshyn <kakowalyshyn@davidson.edu>
2023-07-11 16:07:01 +0200

    Split YAML witness file parsing into multiple methods

--- trunk/source/WitnessParser/src/de/uni_freiburg/informatik/ultimate/witnessparser/YAMLWitnessParser.java
+++ trunk/source/WitnessParser/src/de/uni_freiburg/informatik/ultimate/witnessparser/YAMLWitnessParser.java
@@ -2,9 +2,14 @@ package de.uni_freiburg.informatik.ultimate.witnessparser;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 
+import com.amihaiemil.eoyaml.Node;
 import com.amihaiemil.eoyaml.Yaml;
+import com.amihaiemil.eoyaml.YamlInput;
+import com.amihaiemil.eoyaml.YamlMapping;
+import com.amihaiemil.eoyaml.YamlNode;
 
 import de.uni_freiburg.informatik.ultimate.core.model.models.ModelType;
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
@@ -14,42 +19,63 @@ import de.uni_freiburg.informatik.ultimate.witnessparser.yaml.LocationInvariant;
 import de.uni_freiburg.informatik.ultimate.witnessparser.yaml.LoopInvariant;
 
 public class YAMLWitnessParser {
-	
-	//how to take file as an expression.. use contains to find invariants or something else?
+	private YamlMapping mWitnessContentMapping;
+	// how to take file as an expression.. use contains to find invariants or something else?
 	public YAMLWitnessParser() throws FileNotFoundException {
-		Yaml.createYamlInput(new File("/home/katie/witness.yml"));
-		Yaml.createYamlInput("-bla\n- blub");
-		// Auto-generated constructor stub
+		
+		//Yaml.createYamlInput("-bla\n- blub"); // Current placeholder in case of test
+		
+		// initializes witnessContent, which is a file holding all of the YAML file 
+		YamlInput witnessContent = Yaml.createYamlInput(new File("/home/katie/sv-witnesses/trex04.invariant_witness.yml"));
+		try {
+			mWitnessContentMapping = witnessContent.readYamlMapping();
+		} catch (IOException e) { //use try/catch in order to ensure that it is read
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void parseWitnessEntries() {
+		//this function parses all entries of the YAML file and saves one as a variable mSequence
+		mWitnessContentMapping.witnessContent();  
+		//mWitnessContentMapping.yamlSequence(null) // placeholder only used in case needed for testing
+		String mSequence = mWitnessContentMapping.yamlSequence(0); //can't use integers
+
 	}
 
-	if (home/katie/witness.yml.contains("location invariant")) { //wrong if statement for this -- want to write: if YML file has this...
-		String mRequirement;
-		String mFormat;
-		String mType;
-		Location mLocation;
+	public void parseWitnessEntry(YamlNode entry) {
 		
-		new LocationInvariant(mRequirement, mFormat, mType, mLocation);
-	}
-	
-	if (home/katie/witness.yml.contains("loop invariant")) {
-		String mRequirement;
-		String mFormat;
-		String mType;
-		Location mLocation;
+		assert(entry.type() == Node.MAPPING);
+
+		final  String entry_type_value = entry.asMapping().string("entry_type");
 		
-		new LoopInvariant(mRequirement, mFormat, mType, mLocation);
+		if (entry_type_value == "location_invariant") { 
+			String mRequirement;
+			String mFormat;
+			String mType;
+			Location mLocation;
+
+			new LocationInvariant(mRequirement, mFormat, mType, mLocation);
+		}
+
+		if (entry_type_value == "loop_invariant") {
+			String mRequirement;
+			String mFormat;
+			String mType;
+			Location mLocation;
+
+			new LoopInvariant(mRequirement, mFormat, mType, mLocation);
+		}
+
+		else if (entry_type_value == "location") {
+			String mName;
+			int mLine;
+			int mColumn;
+			String mFileName;
+			String mUuid;
+
+			new Location(mName, mLine, mColumn, mFileName, mUuid);
+		}
 	}
-	
-	if (home/katie/witness.yml.contains("location")) {
-		String mName; 
-		int mLine;
-		int mColumn;
-		String mFileName;
-		String mUuid;
-		
-		new Location(mName, mLine, mColumn, mFileName, mUuid);
-	}
-	
-	
-  
+
 }
